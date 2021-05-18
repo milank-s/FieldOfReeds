19a5c5c89a60886e6053d4ec06f2fc350fd89a2a
Milan Koerner-Safrata <mks554@nyu.edu>
2021-05-18 17:50:30 -0400

    clamping mic volume on readout

--- Assets/Scripts/LineReadout.cs
+++ Assets/Scripts/LineReadout.cs
@@ -18,17 +18,21 @@ public class LineReadout : MonoBehaviour
     public void Start(){
         line.positionCount = segments;
     }
-
     public void SetValue(float value){
         amplitude = value;
     }
     public void UpdateLine()
     {   
+        float adjustedAmplitude = amplitude * magnitude;
+
+        if(adjustedAmplitude > 1){
+           adjustedAmplitude /= adjustedAmplitude;
+        }
         for(int i = 0; i < segments; i++){
             float normalized = (float)i/(float)segments;
             normalized = (normalized - 0.5f) * 2f;
             float height = Mathf.PerlinNoise((float)frequency * i + Time.time * speed, 0);
-            height = ((height*2) -1) * magnitude * amplitude;
+            height = ((height*2) -1) * adjustedAmplitude;
             Vector3 position = new Vector3(width * normalized, height, 0);
             line.SetPosition(i, position + transform.position);
         }
